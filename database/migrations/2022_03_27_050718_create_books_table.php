7a5189ed3d491b82f23aabe55b32e5390ab2a7c0
Moumen Alisawe <muisawe@gmail.com>
2022-03-27 08:16:14 +0300

    init migrations

---
+++ database/migrations/2022_03_27_050718_create_books_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('avatar')->nullable();
+            $table->string('author');
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+
+
+        Schema::create('favorite', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Book::class);
+
+            $table->timestamps();
+        });
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Book::class);
+            $table->tinyInteger('rate');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
